63aab50080b626b4c893f23f4a2f573d007d90ec
alicewish <anywaywillgo@gmail.com>
2016-06-19 15:08:28 +0800

    Update

--- 安装说明.py
+++ 安装说明.py
@@ -1,8 +1,20 @@
-# pyenv install anaconda3-4.0.0
+#========================安装环境的命令========================
+#  pyenv install anaconda3-4.0.0
 # pyenv install 3.5.1
 # pyenv global anaconda-4.0.0
 # pyenv global 3.5.1
 #
+#========================安装库的命令========================
+#==================Mac==================
 # cd /Users/alicewish/Downloads/Compressed/python-docx-0.8.5
 #
 # python  setup.py install  --prefix=/Users/alicewish/anaconda/
+#
+# pip install --target=/Users/alicewish/anaconda/ qiniu
+
+#==================Win==================
+# pip install --target=C:\Anaconda3\ qiniu
+
+
+#========================重要的库========================
+#html2text
